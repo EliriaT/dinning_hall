1fff15251580e027a3fe383dec4e8335a3c0bb4f
IrinaT <irina.tiora@isa.utm.md>
2022-09-27 20:09:50 +0300

    Add adjustments

--- dinning-hall-elem/config.go
+++ dinning-hall-elem/config.go
@@ -2,5 +2,9 @@ package dinning_hall_elem
 
 import "time"
 
-const TimeUnit = time.Duration(float64(time.Millisecond) * 50)
-const OrdersLimit = 50
+const (
+	TimeUnit    = time.Duration(float64(time.Millisecond) * 50)
+	OrdersLimit = 100
+
+	URL = "http://localhost:8080/order"
+)
